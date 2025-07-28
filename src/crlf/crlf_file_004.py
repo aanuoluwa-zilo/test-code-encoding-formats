4b69983ae7f59e466622400db0b1e45b9e4a4191
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 17:43:34 +0100

    Add encoding format test file: src/crlf/crlf_file_004.py

---
+++ src/crlf/crlf_file_004.py
@@ -0,0 +1,8 @@
+# CRLF line endings file 4
+# This file uses CRLF line endings
+class CRLFProcessor4:
+    def __init__(self):
+        self.name = "CRLF File"
+    
+    def process_crlf(self):
+        return "CRLF processed"
