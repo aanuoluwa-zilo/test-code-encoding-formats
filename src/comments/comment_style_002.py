189804b288e71ec379ad4701fa91901d01fa763f
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 17:43:47 +0100

    Add encoding format test file: src/comments/comment_style_002.py

---
+++ src/comments/comment_style_002.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# Comment style file 2
+# This file tests different comment encoding styles
+"""
+class CommentProcessor2:
+    # Single line comment
+    def __init__(self):
+        self.name = "Comment File"  # Inline comment
+    
+    def process_comments(self):
+        """
+        Multi-line comment
+        with multiple lines
+        """
+        return "Comment processed"
